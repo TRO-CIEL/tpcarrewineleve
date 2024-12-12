573b5ec8f32386bf1dba3fea2b797fcf595ece88
Téo Trompier <teo.trompier@gmail.com>
2024-12-12 11:28:32 +0100

    Commit #4

--- Form1.h
+++ Form1.h
@@ -116,7 +116,7 @@ namespace tpcarrewineleve {
 			pcarre1->Setsy(0); // Repositionne en haut
 		}
 
-		pcarre1->Animer(this); // Animation
+		pcarre1->Animer(this); // Animation du carré
 	}
 	};
 }
